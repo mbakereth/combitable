7a9115c035405303fccc972c7d4c15a1e7a14b68
Matthew Baker <matt@mac2.local>
2025-06-22 09:31:16 +0200

    Export DetailsField

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 // Copyright (c) 2024 Matthew Baker.  All rights reserved.  Licenced under the Apache Licence 2.0.  See LICENSE file
 
 export { default as CombiTable } from '$lib/components/CombiTable.svelte';
+export { default as DetailsField } from '$lib/components/DetailsField.svelte';
 export type { CombiTableColumn, CombiTablePresets, PrismaWhereFunction } from '$lib/combitabletypes';
 export { SearchUrl, type PrismaFields } from '$lib/searchurl';
 export {default as upIcon} from "$lib/assets/prime--sort-up-fill.svg?raw"
